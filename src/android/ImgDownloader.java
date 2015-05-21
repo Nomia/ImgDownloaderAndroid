9897b56d826236783556d17eb47246cb0a26c3bf
Vincent <vincent@okay.do>
2015-05-21 20:08:17 +0800

    bug fix, finished download method, download file to sdcard/DCIM/Camera

--- src/android/ImgDownloader.java
+++ src/android/ImgDownloader.java
@@ -1,11 +1,3 @@
-/**
- * Phonegap ClipboardManager plugin
- * Omer Saatcioglu 2011
- * enhanced by Guillaume Charhon - Smart Mobile Software
- * ported to Phonegap 2.0 by Jacob Robbins
- * ported to Phonegap 3.0 by Guillaume Charhon - Smart Mobile Software 
- */
-
 package com.okay.phonegap.ImgDownloader;
 
 import org.json.JSONArray;
@@ -16,6 +8,8 @@ import android.content.Context;
 import java.util.Calendar;
 import android.content.Intent;
 import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -28,6 +22,9 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
 
+import java.lang.reflect.Method;
+import android.os.storage.StorageManager;
+
 
 public class ImgDownloader extends CordovaPlugin {
     @Override
@@ -35,34 +32,127 @@ public class ImgDownloader extends CordovaPlugin {
             throws JSONException {
         if("download".equals(action)) {
             this.download(args.getString(0), callbackContext);
-            callbackContext.success();
             return true;
         }
         return false;
     }
      
-    private void download(String filePath, CallbackContext callbackContext) {
+    private Boolean download(String filePath, CallbackContext callbackContext) {
         String errorText = "";
         String[] filePathParts = filePath.split("://");
         filePath = filePathParts[1];
-        File ext =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        try {
-            FileChannel inChannel = new FileInputStream(filePath).getChannel();
-            File outFile = new File(ext, filenameFromCurrentDate()+".jpg");
-            FileChannel outChannel = new FileOutputStream(outFile).getChannel();
-            try {
-                inChannel.transferTo(0,inChannel.size(),outChannel);
+
+        String outpath;
+
+        boolean sdCardExist = Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
+        String filename = filenameFromCurrentDate()+".jpg";
+
+        //if sdcard exist,we save it to sdcard
+        if(sdCardExist){
+            String sdpath = this.getSecondaryStoragePath();
+            if(sdpath != null){
+                outpath = sdpath + "/DCIM/Camera";
+
+                File cameraDir = new File(outpath);
+                if(cameraDir.exists()){
+                    this.copyFile(filePath,filename,outpath,callbackContext);
+                    return true;
+                }
             }
-            finally {
-                if(inChannel!=null) inChannel.close();
-                if(outChannel!=null) outChannel.close();
-            }
-            scanPhoto(outFile.getPath());
-            callbackContext.success(outFile.getPath());
         }
-        catch(IOException e) {
-            callbackContext.error("error: "+errorText);
-            e.printStackTrace();
+         
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+
+        if(dir.exists()){
+            outpath = dir.getAbsolutePath() + "/Camera";
+        }else{
+            outpath =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath();
+        }
+
+
+        this.copyFile(filePath,filename,outpath,callbackContext);
+        return true;
+        // File ext =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        // try {
+        //     FileChannel inChannel = new FileInputStream(filePath).getChannel();
+        //     File outFile = new File(ext, filenameFromCurrentDate()+".jpg");
+        //     FileChannel outChannel = new FileOutputStream(outFile).getChannel();
+            
+        //     try {
+        //         inChannel.transferTo(0,inChannel.size(),outChannel);
+        //     }
+        //     finally {
+        //         if(inChannel!=null) inChannel.close();
+        //         if(outChannel!=null) outChannel.close();
+        //     }
+            
+        //     scanPhoto(outFile.getPath());
+        //     callbackContext.success(outFile.getPath());
+        //     return outFile.getPath();
+        // }
+        // catch(IOException e) {
+        //     callbackContext.error("error: "+errorText);
+        //     e.printStackTrace();
+
+        //     return "";
+        // }
+    }
+
+    public String getSecondaryStoragePath() {
+        try {
+            StorageManager sm = (StorageManager) cordova.getActivity().getSystemService(Context.STORAGE_SERVICE);
+            Method getVolumePathsMethod = StorageManager.class.getMethod("getVolumePaths", null);
+            String[] paths = (String[]) getVolumePathsMethod.invoke(sm, null);
+            // second element in paths[] is secondary storage path
+            return paths[1];
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+
+    private Boolean copyFile(String inputFilePath, String inputFile, String outputPath,CallbackContext callbackContext) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+
+            //create output directory if it doesn't exist
+            File dir = new File (outputPath); 
+            if (!dir.exists())
+            {
+                dir.mkdirs();
+            }
+
+            String outputFilePath = outputPath +"/"+ inputFile;
+            in = new FileInputStream(inputFilePath);        
+            out = new FileOutputStream(outputFilePath);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            
+            in = null;
+
+            File file = new File(outputFilePath);
+            if(file.exists()){
+                callbackContext.success(outputFilePath);
+            }else{
+                callbackContext.error("error");
+            }
+
+                // write the output file (You have now copied the file)
+            out.flush();
+            out.close();
+            out = null;        
+            return true;
+        }catch (FileNotFoundException fnfe1) {
+            return false;
+        }catch (Exception e) {
+            return false;
         }
     }
      
